f2d2d819133dd84b8725c4c2cb172be09ddb4509
sunabove <sunabove@nate.com>
2024-11-08 10:56:36 +0900

    **

--- MyProject/src/Ex010_IfExample.java
+++ MyProject/src/Ex010_IfExample.java
@@ -8,7 +8,20 @@ public class Ex010_IfExample {
 			System.out.println( "b" ) ;
 		} else {
 			System.out.println( "c" ) ;
-		}
+		} ; 
+		
+		int a = 1 ;
+		int b = 2 ; 
+		
+		if( a > b ) { 
+			System.out.println( "c" ) ;
+		} ; 
+		
+		if( 1 > 2 ) { 
+		} else {
+			System.out.println( "c" ) ;
+		} ; 
+		
 	}
 
 }
--- MyProject/src/Ex012_ForExample.java
+++ MyProject/src/Ex012_ForExample.java
@@ -1,8 +1,9 @@
 public class Ex012_ForExample {
     public static void main(String[] args) {
         // 1부터 10까지 출력
-        for (int i = 1; i <= 10; i++) {
+    	
+        for( int i = 1 ; i <= 10000000 ; i ++ ) {
             System.out.println("Number: " + i);
         }
     }
-}
+} 
