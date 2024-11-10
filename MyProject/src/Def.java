11d0a6443ba3251d2f80a0df3484bef6853c4c2c
sunabove <sunabove@nate.com>
2024-11-11 05:20:46 +0900

    **

--- MyProject/src/Def.java
+++
@@ -1,8 +0,0 @@
-
-public class Def {
-
-	public static void main(String[] args) { 
-
-	}
-
-}
---
+++ MyProject/src/Ex024_WhileExample.java
@@ -0,0 +1,10 @@
+public class Ex024_WhileExample {
+    public static void main(String[] args) {
+        int count = 1;
+
+        while( count <= 10 ) {
+            System.out.println("Count: " + count);
+            count++; 
+        }
+    }
+}
