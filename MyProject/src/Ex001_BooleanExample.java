e871956f8c67d3a35aac511f48191cdcd4045349
sunabove <sunabove@nate.com>
2024-11-11 04:23:46 +0900

    **

---
+++ MyProject/src/Ex001_BooleanExample.java
@@ -0,0 +1,17 @@
+public class Ex001_BooleanExample {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+
+        System.out.println("Is Java fun? " + isJavaFun); 
+        System.out.println("Is fish tasty? " + isFishTasty); 
+
+        boolean isRaining = true;
+
+        if (isRaining) {
+            System.out.println("Take an umbrella.");
+        } else {
+            System.out.println("Enjoy the sunshine!");
+        }
+    }
+}
