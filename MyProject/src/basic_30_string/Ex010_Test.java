0a5a5fead3bc23dbfff4eba51521b43ac4351e6d
sunabove <sunabove@nate.com>
2024-11-12 19:57:27 +0900

    **

--- MyProject/src/basic_30_string/Ex010_Test.java
+++ MyProject/src/basic_30_string/Ex010_Test.java
@@ -2,6 +2,8 @@ package basic_30_string;
 public class Ex010_Test {
     public static void main(String[] args) {
     	
+    	java.lang.Math.sqrt( 2 ) ; 
+    	
     	boolean b = ( 0.1 + 0.2 == 0.3 );
     	
     	System.out.println( b ) ; 
