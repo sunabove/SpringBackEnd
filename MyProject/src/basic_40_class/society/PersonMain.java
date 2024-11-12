0e096ce05b6bfa4cfe9ce4f6e17cb60dff225571
sunabove <sunabove@nate.com>
2024-11-13 06:25:51 +0900

    Update PersonMain.java

--- MyProject/src/basic_40_class/society/PersonMain.java
+++ MyProject/src/basic_40_class/society/PersonMain.java
@@ -6,6 +6,12 @@ public class PersonMain {
 		// Person 객체 생성
 		Person person = new Person( "Alice", 25 );
 		
+		int age = person.getAge() ;
+        System.out.println( "age = " + age );
+        
+        String name = person.getName();
+        System.out.println( "name = " + name );
+        
         person.showInfo();
         
         // 생일을 맞이함.
