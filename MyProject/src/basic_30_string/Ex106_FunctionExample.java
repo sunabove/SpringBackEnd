c29db7f02c7b579540b98993d3a7bd42aba5eda8
sunabove <sunabove@nate.com>
2024-11-13 09:18:44 +0900

    **

--- MyProject/src/basic_30_string/Ex106_FunctionExample.java
+++ MyProject/src/basic_30_string/Ex106_FunctionExample.java
@@ -1,19 +1,14 @@
 package basic_30_string;
+
 public class Ex106_FunctionExample {
 
-    // 짝수일 경우 "짝수"를 출력하고 함수 종료, 홀수일 경우 계속 실행
-    public static void checkEvenOrOdd(int number) {
-        if (number % 2 == 0) {
-            System.out.println("짝수");
-            return;  // 짝수인 경우, 함수 실행 종료
-        }
-
-        // 홀수인 경우 실행되는 코드
-        System.out.println("홀수");
+	// 두 정수를 더하는 함수
+    public static int add(int a, int b) {
+        return a + b;  // 두 수의 합을 반환
     }
 
     public static void main(String[] args) {
-        checkEvenOrOdd(4);  // 짝수 출력 후 함수 종료
-        checkEvenOrOdd(7);  // 홀수 출력
+        int result = add(5, 10);  // add 함수 호출
+        System.out.println("합: " + result);  // 출력: 합: 15
     }
 }
