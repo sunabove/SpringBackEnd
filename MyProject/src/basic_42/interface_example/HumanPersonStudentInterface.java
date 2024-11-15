fe3bbf3826ae0c72501dadb9bf3f2b5790378789
sunabove <sunabove@nate.com>
2024-11-15 09:01:27 +0900

    Update HumanPersonStudentInterface.java

--- MyProject/src/basic_42/interface_example/HumanPersonStudentInterface.java
+++ MyProject/src/basic_42/interface_example/HumanPersonStudentInterface.java
@@ -1,7 +1,7 @@
 package basic_42.interface_example;
 
 //Human 인터페이스 정의 - 공통적인 특성을 정의
-interface Human {
+interface HumanPersonStudentInterface {
 	// 이름을 반환하는 메소드
 	String getName();
 
@@ -10,7 +10,7 @@ interface Human {
 }
 
 //Person 클래스 - Human 인터페이스 구현
-class Person implements Human {
+class Person implements HumanPersonStudentInterface {
 	protected String name;
 	protected int age;
 
@@ -33,7 +33,7 @@ class Person implements Human {
 }
 
 //Student 클래스 - Human 인터페이스 구현, Person과 다른 특성 추가
-class Student implements Human {
+class Student implements HumanPersonStudentInterface {
 	private String name;
 	private int age;
 	private String studentId;
